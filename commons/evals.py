1e2a385b330f1a54f17929834b2e20abfda7a151
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-01-29 21:44:42 +0800

    refactor: classification accuracy eval, remove softmax since outputs from same model

--- commons/evals.py
+++ commons/evals.py
@@ -1,38 +1,51 @@
 import torch
 from torch.utils.data import DataLoader
-from commons.models import ModelZoo, get_cached_model, get_cached_tokenizer
+from commons.models import ModelName, get_cached_model, get_cached_tokenizer
+
 from datasets import load_dataset
-from torch.nn import functional as F
+import bittensor as bt
 
-dataset = load_dataset("Anthropic/hh-rlhf")
-dataset = dataset.with_format("torch")
-batch_size = 16
-split = "train"
-dataloader = DataLoader(dataset[split], batch_size=batch_size)
 
-model_name = ModelZoo.DEBERTA_V3_LARGE_V2
-model = get_cached_model(model_name)
-tokenizer = get_cached_tokenizer(model_name)
-total_chosen = 0
-for idx, batch in enumerate(dataloader):
-    # TODO check if need to split the raw string into dialogues
-    chosen = tokenizer(
-        batch["chosen"], return_tensors="pt", padding=True, truncation=True
-    )
-    rejected = tokenizer(
-        batch["rejected"], return_tensors="pt", padding=True, truncation=True
-    )
+def get_dataloader(hf_repo_id: str, **kwargs):
+    dataset = load_dataset(hf_repo_id, streaming=True)
+    dataset = dataset.with_format("torch")
+    if (split := kwargs.get("split", None)) is not None:
+        dataset = dataset[split]
+    if "format" in kwargs:
+        dataset = dataset.with_format(kwargs["format"])
+    return DataLoader(dataset, batch_size=16)
 
-    chosen_logits = model(**chosen).logits
-    rejected_logits = model(**rejected).logits
-    # cat over batch dim, for 'chosen' and 'rejected'
-    logits = torch.cat([chosen_logits, rejected_logits], dim=1)
-    # check if chosen is more likely than rejected
-    probs = F.softmax(logits, dim=-1)
 
-    values, indices = torch.max(probs, dim=-1)
-    num_chosen = sum(indices == 0)
-    total_chosen += num_chosen
+class EvalUtils:
+    @staticmethod
+    def classification_accuracy(model_name: ModelName) -> float:
+        split = "train"
+        dataset = get_dataloader("Anthropic/hh-rlhf", split=split, format="torch")
+        accuracy = calculate_classification_accuracy(dataset, model_name, split=split)
+        return accuracy
 
-accuracy = total_chosen / len(dataset[split])
-print(f"Accuracy: {accuracy}")
+
+def calculate_classification_accuracy(dataset, model_name):
+    model = get_cached_model(model_name)
+    tokenizer = get_cached_tokenizer(model_name)
+    total_chosen = 0
+    for idx, batch in enumerate(dataset):
+        # TODO check if need to split the raw string into dialogues
+        chosen = tokenizer(
+            batch["chosen"], return_tensors="pt", padding=True, truncation=True
+        )
+        rejected = tokenizer(
+            batch["rejected"], return_tensors="pt", padding=True, truncation=True
+        )
+
+        chosen_logits = model(**chosen).logits
+        rejected_logits = model(**rejected).logits
+        logits = torch.cat([chosen_logits, rejected_logits], dim=1)
+        # check if chosen is more likely than rejected
+        values, indices = torch.max(logits, dim=-1)
+        num_chosen = sum(indices == 0)
+        total_chosen += num_chosen
+
+    accuracy = total_chosen / len(dataset)
+    bt.logging.info(f"Accuracy: {accuracy}")
+    return accuracy
