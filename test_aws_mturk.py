f586335c2187374ac7ef226c6d422a19e727e843
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-02-09 23:34:16 +0800

    feat: update aws mturk script for task creation

--- test_aws_mturk.py
+++ test_aws_mturk.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple
 import boto3
 
 import textwrap
@@ -7,24 +7,54 @@ import os
 
 from commons.llm.prompts import ScoreRange
 from template.protocol import Completion
+import bittensor as bt
+import botocore.exceptions
 
 load_dotenv()
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 MTURK_SANDBOX = "https://mturk-requester-sandbox.us-east-1.amazonaws.com"
+# MTURK_SANDBOX = "https://mturk-requester.us-east-1.amazonaws.com"
+US_EAST_REGION = "us-east-1"
+# ensure regions in 'endpoint' key matches
+environment_dict = {
+    "production": {
+        "endpoint_url": "https://mturk-requester.us-east-1.amazonaws.com",
+        "preview_url": "https://www.mturk.com/mturk/preview",
+    },
+    "sandbox": {
+        "endpoint_url": "https://mturk-requester-sandbox.us-east-1.amazonaws.com",
+        "preview_url": "https://workersandbox.mturk.com/mturk/preview",
+    },
+}
+
+
+def get_environment_config(environment: str) -> Dict[str, Any]:
+    if environment not in environment_dict:
+        raise ValueError(f"Invalid environment: {environment}")
+
+    bt.logging.info("Using AWS environment: " + environment)
+    return environment_dict[environment]
+
+
+env_config = get_environment_config("sandbox")
 
 mturk_client = boto3.client(
     "mturk",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_KEY,
-    region_name="us-east-1",
-    endpoint_url=MTURK_SANDBOX,
+    region_name=US_EAST_REGION,
+    endpoint_url=env_config["endpoint_url"],
 )
-print(
-    "I have $"
-    + mturk_client.get_account_balance()["AvailableBalance"]
-    + " in my Sandbox account"
-)
+
+
+def get_balance():
+    print(
+        "I have $"
+        + mturk_client.get_account_balance()["AvailableBalance"]
+        + " in my Sandbox account"
+    )
+    return mturk_client.get_account_balance()["AvailableBalance"]
 
 
 def _build_description(num_completions):
@@ -69,10 +99,7 @@ def _build_sliders_html(completion_ids: List[str], score_range: ScoreRange):
     return "<ul>" + "\n".join(list_items) + "</ul>"
 
 
-# TODO based on prompt and completions build xhtml content
-
-
-def build_task_xhtml_content(
+def _build_task_xhtml_content(
     prompt: str, completions: List[Completion], score_range: ScoreRange
 ):
     xhtml_content = f"""
@@ -82,13 +109,22 @@ def build_task_xhtml_content(
         <body>
             <script src="https://assets.crowd.aws/crowd-html-elements.js"></script>
             <crowd-form>
-                <p><strong>{_build_instruction(num_completions=len(completions), score_range=score_range)}</strong></p>
+                <p><strong>
+                {_build_instruction(num_completions=len(completions), score_range=score_range)}
+                </strong></p>
+
                 <p><strong>Prompt: </strong>{prompt}</p>
+
                 {_build_completions_html([completion.text for completion in completions])}
 
-                <p><strong>Score each response here.</strong>
+                <p><strong>Score each response here!</strong>
                     {_build_sliders_html([completion.cid for completion in completions], score_range)}
                 </p>
+
+            <crowd-toast duration="10000" opened>
+            Thank you for your interest in our task. By completing this task, you will be contributing to the future of Artificial Intelligence!
+            </crowd-toast>
+
             </crowd-form>
         </body>
         </html>
@@ -99,76 +135,47 @@ def build_task_xhtml_content(
     return xhtml_content
 
 
-score_range = ScoreRange(lower=1, upper=10)
-prompt = "What is your name?"
-completions = [
-    Completion(text="My name is Alice, nice to meet you!"),
-    Completion(text="My name is Bob and I like apples."),
-]
+# TODO take these as parameters
+def create_mturk_task():
+    """Create a human intellgence task to send to AWS MTurk workers."""
+    score_range = ScoreRange(lower=1, upper=10)
 
-new_hit = mturk_client.create_hit(
-    Title="Is this Tweet happy, angry, excited, scared, annoyed or upset?",
-    Description=_build_description(5),
-    Keywords="text, quick, labeling, scoring, easy, bonus",
-    Reward=str(0.15),
-    MaxAssignments=1,
-    LifetimeInSeconds=3600,
-    AssignmentDurationInSeconds=600,
-    AutoApprovalDelayInSeconds=3600 * 48,
-    Question=build_task_xhtml_content(prompt, completions, score_range),
-)
-print("A new HIT has been created. You can preview it here:")
-print(
-    "https://workersandbox.mturk.com/mturk/preview?groupId="
-    + new_hit["HIT"]["HITGroupId"]
-)
-print("HITID = " + new_hit["HIT"]["HITId"] + " (Use to Get Results)")
-# Remember to modify the URL above when you're publishing
-# HITs to the live marketplace.
-# Use: https://worker.mturk.com/mturk/preview?groupId=
+    # TODO take in real data after testing
+    prompt = "What is your name?"
+    completions = [
+        Completion(text="My name is Alice, nice to meet you!"),
+        Completion(text="My name is Bob and I like apples."),
+    ]
+
+    max_num_mturk_workers = 3
+    payout_auto_approval_seconds = 3600 * 48
+    search_keywords = ", ".join(
+        ["text", "quick", "labeling", "scoring", "easy", "bonus"]
+    )
+    try:
+        new_hit = mturk_client.create_hit(
+            Title="Prompt & Completion Evaluation Task",
+            Description=_build_description(len(completions)),
+            Keywords=search_keywords,
+            Reward=str(0.01),
+            MaxAssignments=max_num_mturk_workers,
+            LifetimeInSeconds=3600,
+            AssignmentDurationInSeconds=600,
+            AutoApprovalDelayInSeconds=payout_auto_approval_seconds,
+            Question=_build_task_xhtml_content(prompt, completions, score_range),
+        )
+        print("A new HIT has been created. You can preview it here:")
+        print(
+            "https://workersandbox.mturk.com/mturk/preview?groupId="
+            + new_hit["HIT"]["HITGroupId"]
+        )
+        print("HITID = " + new_hit["HIT"]["HITId"] + " (Use to Get Results)")
+    except botocore.exceptions.ClientError as e:
+        print(f"Error occurred while trying to create hit... exception: {e}")
 
 
-################################ STORING AWS XHTML CONTENT SAMPLES
-
-
-def get_sentiment_classification_example():
-    return """
-    <HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
-      <HTMLContent><![CDATA[
-        <!DOCTYPE html>
-          <body>
-            <script src="https://assets.crowd.aws/crowd-html-elements.js"></script>
-            <crowd-form>
-              <crowd-classifier
-                name="sentiment"
-                categories="['Positive', 'Negative', 'Neutral', 'N/A']"
-                header="What sentiment does this text convey?"
-              >
-                <classification-target>
-                Everything is wonderful.
-                </classification-target>
-
-                <full-instructions header="Sentiment Analysis Instructions">
-                <p><strong>Positive</strong>
-                  sentiment include: joy, excitement, delight</p>
-                <p><strong>Negative</strong> sentiment include:
-                  anger, sarcasm, anxiety</p>
-                <p><strong>Neutral</strong>: neither positive or
-                  negative, such as stating a fact</p>
-                <p><strong>N/A</strong>: when the text cannot be
-                  understood</p>
-                <p>When the sentiment is mixed, such as both joy and sadness,
-                  use your judgment to choose the stronger emotion.</p>
-                </full-instructions>
-
-                <short-instructions>
-                 Choose the primary sentiment that is expressed by the text.
-                </short-instructions>
-              </crowd-classifier>
-            </crowd-form>
-          </body>
-        </html>
-      ]]></HTMLContent>
-      <FrameHeight>0</FrameHeight>
-    </HTMLQuestion>
-    """
+if __name__ == "__main__":
+    # send_test_event_notification()
+    create_mturk_task()
+    print("Done...")
+    pass
