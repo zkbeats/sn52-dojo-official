99ff4f2785a57509882099832616f4bf995ce04f
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-02-10 00:10:09 +0800

    feat: group all functions under MTurkUtils, add event handler placeholder

--- commons/human_feedback/aws_mturk.py
+++ commons/human_feedback/aws_mturk.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, List, Tuple
+import json
+from typing import Any, Dict, List
 import boto3
 
 import textwrap
@@ -13,11 +14,10 @@ import botocore.exceptions
 load_dotenv()
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
-MTURK_SANDBOX = "https://mturk-requester-sandbox.us-east-1.amazonaws.com"
-# MTURK_SANDBOX = "https://mturk-requester.us-east-1.amazonaws.com"
 US_EAST_REGION = "us-east-1"
 # ensure regions in 'endpoint' key matches
-environment_dict = {
+
+mturk_env_dict = {
     "production": {
         "endpoint_url": "https://mturk-requester.us-east-1.amazonaws.com",
         "preview_url": "https://www.mturk.com/mturk/preview",
@@ -30,11 +30,11 @@ environment_dict = {
 
 
 def get_environment_config(environment: str) -> Dict[str, Any]:
-    if environment not in environment_dict:
+    if environment not in mturk_env_dict:
         raise ValueError(f"Invalid environment: {environment}")
 
     bt.logging.info("Using AWS environment: " + environment)
-    return environment_dict[environment]
+    return mturk_env_dict[environment]
 
 
 env_config = get_environment_config("sandbox")
@@ -48,134 +48,174 @@ mturk_client = boto3.client(
 )
 
 
-def get_balance():
-    print(
-        "I have $"
-        + mturk_client.get_account_balance()["AvailableBalance"]
-        + " in my Sandbox account"
-    )
-    return mturk_client.get_account_balance()["AvailableBalance"]
+class MTurkUtils:
+    @staticmethod
+    def encode_task_key(completion_id: str):
+        """Simple method to take Completion.cid and encode it in a way that we can receive it from AWS Lambda"""
+        return f"cid_{completion_id}"
 
+    @staticmethod
+    def decode_task_key(encoded_key: str) -> str:
+        """Simple method to decode the key from AWS Lambda, to get the corresponding completion UUID as string"""
+        return encoded_key.split("_")[1]
 
-def _build_description(num_completions):
-    description = f"""
-    This is a task where you will need to read up to {num_completions} sentences.
-    You will need to score each prompt and sentence and score them on a scale of 1 to 10 in terms of quality.
-    """
-    return textwrap.dedent(description)
+    @staticmethod
+    def get_balance():
+        return mturk_client.get_account_balance()["AvailableBalance"]
 
+    @staticmethod
+    def create_mturk_task(
+        prompt: str,
+        completions: List[Completion],
+        score_range: ScoreRange = ScoreRange(lower=1, upper=10),
+        title: str = "Prompt & Completion Evaluation Task",
+        max_num_workers: int = 3,
+        search_keywords: List[str] = [
+            "text",
+            "quick",
+            "labeling",
+            "scoring",
+            "easy",
+            "bonus",
+        ],
+        reward_in_dollars: float = 0.01,
+    ):
+        """Create a human intellgence task to send to AWS MTurk workers."""
+        payout_auto_approval_seconds = 3600 * 24
+        try:
+            new_hit = mturk_client.create_hit(
+                Title=title,
+                Description=MTurkUtils.build_description(len(completions)),
+                Keywords=", ".join(search_keywords),
+                Reward=str(reward_in_dollars),
+                MaxAssignments=max_num_workers,
+                LifetimeInSeconds=3600,
+                AssignmentDurationInSeconds=600,
+                AutoApprovalDelayInSeconds=payout_auto_approval_seconds,
+                Question=MTurkUtils.build_task_xhtml_content(
+                    prompt, completions, score_range
+                ),
+            )
+            hit_url = (
+                f"{env_config['preview_url']}?groupId={new_hit['HIT']['HITGroupId']}"
+            )
+            bt.logging.info(
+                f"A new HIT has been created. You can preview it here:\n{hit_url}"
+            )
+            bt.logging.info(
+                "HITID = " + new_hit["HIT"]["HITId"] + " (Use to Get Results)"
+            )
+        except botocore.exceptions.ClientError as e:
+            bt.logging.error(
+                f"Error occurred while trying to create hit... exception: {e}"
+            )
 
-def _build_instruction(num_completions, score_range: ScoreRange):
-    instruction = f"""
-    This is a task where you will need to read up to {num_completions} sentences.<br>
-    You will need to score each prompt and sentence and score them on a scale of 1 to 10 in terms of quality.<br>
-    Rate the following texts on a scale of {score_range.lower} to {score_range.upper}, where {score_range.lower} is the lowest quality and {score_range.upper} is the highest quality.
-    """
-    return textwrap.dedent(instruction)
+    @staticmethod
+    def handle_mturk_event(payload: Any):
+        # json_payload = None
+        # if isinstance(payload, str):
+        #     json_payload = json.loads(payload)
+        # elif isinstance(payload, dict):
+        #     json_payload = payload
+        # elif isinstance(payload, list):
+        #     json_payload = payload
 
+        # assert json_payload is not None, f"Unexpected payload type {type(payload)}"
 
-def _build_completions_html(completions: List[str]) -> str:
-    html_repr = "\n".join(
-        [
-            f"<p><strong>Response #{index + 1}: </strong>{completion}</p>"
-            for index, completion in enumerate(completions)
-        ]
-    )
-    return html_repr
+        # TODO handle this event data
+        # [
+        #     {
+        #         "WorkerId": "A3IEQBE35F5IHB",
+        #         "Answer": [
+        #             {
+        #                 "taskAnswers": [
+        #                     {
+        #                         "cid_a6f41ad1-d8a8-4bf3-a698-1b431bf2edac": 5.68,
+        #                         "cid_f95cae4d-38ed-4911-b97a-f92a0c3bad9a": 7.49,
+        #                     }
+        #                 ]
+        #             }
+        #         ],
+        #     }
+        # ]
+        print(f"Payload: {payload}")
 
+        pass
 
-def _build_sliders_html(completion_ids: List[str], score_range: ScoreRange):
-    """Build sliders for each completion, so that we can tie the score back to the request"""
-    list_items = []
-    for idx, cid in enumerate(completion_ids):
-        list_item = f"""
-          <li>
-            <p>Response #{idx+1}</p>
-            <p><crowd-slider name="cid_{cid}" step="0.01" min="{score_range.lower}" max="{score_range.upper}" required pin/></p>
-          </li>
+    @staticmethod
+    def build_description(num_completions):
+        description = f"""
+        This is a task where you will need to read up to {num_completions} sentences.
+        You will need to score each prompt and sentence and score them on a scale of 1 to 10 in terms of quality.
         """
-        list_items.append(textwrap.dedent(list_item))
+        return textwrap.dedent(description)
 
-    return "<ul>" + "\n".join(list_items) + "</ul>"
+    @staticmethod
+    def _build_instruction(num_completions, score_range: ScoreRange):
+        instruction = f"""
+        This is a task where you will need to read up to {num_completions} sentences.<br>
+        You will need to score each prompt and sentence and score them on a scale of 1 to 10 in terms of quality.<br>
+        Rate the following texts on a scale of {score_range.lower} to {score_range.upper}, where {score_range.lower} is the lowest quality and {score_range.upper} is the highest quality.
+        """
+        return textwrap.dedent(instruction)
 
-
-def _build_task_xhtml_content(
-    prompt: str, completions: List[Completion], score_range: ScoreRange
-):
-    xhtml_content = f"""
-<HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
-    <HTMLContent><![CDATA[
-        <!DOCTYPE html>
-        <body>
-            <script src="https://assets.crowd.aws/crowd-html-elements.js"></script>
-            <crowd-form>
-                <p><strong>
-                {_build_instruction(num_completions=len(completions), score_range=score_range)}
-                </strong></p>
-
-                <p><strong>Prompt: </strong>{prompt}</p>
-
-                {_build_completions_html([completion.text for completion in completions])}
-
-                <p><strong>Score each response here!</strong>
-                    {_build_sliders_html([completion.cid for completion in completions], score_range)}
-                </p>
-
-            <crowd-toast duration="10000" opened>
-            Thank you for your interest in our task. By completing this task, you will be contributing to the future of Artificial Intelligence!
-            </crowd-toast>
-
-            </crowd-form>
-        </body>
-        </html>
-    ]]></HTMLContent>
-    <FrameHeight>0</FrameHeight>
-</HTMLQuestion>
-    """
-    return xhtml_content
-
-
-# TODO take these as parameters
-def create_mturk_task():
-    """Create a human intellgence task to send to AWS MTurk workers."""
-    score_range = ScoreRange(lower=1, upper=10)
-
-    # TODO take in real data after testing
-    prompt = "What is your name?"
-    completions = [
-        Completion(text="My name is Alice, nice to meet you!"),
-        Completion(text="My name is Bob and I like apples."),
-    ]
-
-    max_num_mturk_workers = 3
-    payout_auto_approval_seconds = 3600 * 48
-    search_keywords = ", ".join(
-        ["text", "quick", "labeling", "scoring", "easy", "bonus"]
-    )
-    try:
-        new_hit = mturk_client.create_hit(
-            Title="Prompt & Completion Evaluation Task",
-            Description=_build_description(len(completions)),
-            Keywords=search_keywords,
-            Reward=str(0.01),
-            MaxAssignments=max_num_mturk_workers,
-            LifetimeInSeconds=3600,
-            AssignmentDurationInSeconds=600,
-            AutoApprovalDelayInSeconds=payout_auto_approval_seconds,
-            Question=_build_task_xhtml_content(prompt, completions, score_range),
+    @staticmethod
+    def _build_completions_html(completions: List[str]) -> str:
+        html_repr = "\n".join(
+            [
+                f"<p><strong>Response #{index + 1}: </strong>{completion}</p>"
+                for index, completion in enumerate(completions)
+            ]
         )
-        print("A new HIT has been created. You can preview it here:")
-        print(
-            "https://workersandbox.mturk.com/mturk/preview?groupId="
-            + new_hit["HIT"]["HITGroupId"]
-        )
-        print("HITID = " + new_hit["HIT"]["HITId"] + " (Use to Get Results)")
-    except botocore.exceptions.ClientError as e:
-        print(f"Error occurred while trying to create hit... exception: {e}")
+        return html_repr
 
+    @staticmethod
+    def _build_sliders_html(completion_ids: List[str], score_range: ScoreRange):
+        """Build sliders for each completion, so that we can tie the score back to the request"""
+        list_items = []
+        for idx, cid in enumerate(completion_ids):
+            list_item = f"""
+              <li>
+                <p>Response #{idx+1}</p>
+                <p><crowd-slider name="{MTurkUtils.encode_task_key(cid)}" step="0.01" min="{score_range.lower}" max="{score_range.upper}" required pin/></p>
+              </li>
+            """
+            list_items.append(textwrap.dedent(list_item))
 
-if __name__ == "__main__":
-    # send_test_event_notification()
-    create_mturk_task()
-    print("Done...")
-    pass
+        return "<ul>" + "\n".join(list_items) + "</ul>"
+
+    @staticmethod
+    def build_task_xhtml_content(
+        prompt: str, completions: List[Completion], score_range: ScoreRange
+    ):
+        xhtml_content = f"""
+    <HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
+        <HTMLContent><![CDATA[
+            <!DOCTYPE html>
+            <body>
+                <script src="https://assets.crowd.aws/crowd-html-elements.js"></script>
+                <crowd-form>
+                    <p><strong>
+                    {MTurkUtils._build_instruction(num_completions=len(completions), score_range=score_range)}
+                    </strong></p>
+
+                    <p><strong>Prompt: </strong>{prompt}</p>
+
+                    {MTurkUtils._build_completions_html([completion.text for completion in completions])}
+
+                    <p><strong>Score each response here!</strong>
+                        {MTurkUtils._build_sliders_html([completion.cid for completion in completions], score_range)}
+                    </p>
+
+                <crowd-toast duration="10000" opened>
+                Thank you for your interest in our task. By completing this task, you will be contributing to the future of Artificial Intelligence!
+                </crowd-toast>
+
+                </crowd-form>
+            </body>
+            </html>
+        ]]></HTMLContent>
+        <FrameHeight>0</FrameHeight>
+    </HTMLQuestion>
+        """
+        return xhtml_content
