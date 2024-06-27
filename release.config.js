b76fda91a6e972714893cdef80d97c45a9b928d7
codebender <167290009+codebender37@users.noreply.github.com>
2024-06-27 14:00:44 +0700

    chore: added commitizen, semantic-release
    - refactored ignore rule on ruff
    - added config for commitizen to enforce convensional commit standard
    - added config for semantic-release to automate versioning

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: ["main"], // specifies which branches semantic-release should consider for making releases
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
