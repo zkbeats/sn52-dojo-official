774b95f80e40e5189cfd51b1789f07c7d90016e7
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-07-19 12:17:34 +0800

    test: run pytests

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,20 @@
 import nox
 
+SUPPORTED_PYTHON_VERSIONS = ["3.10", "3.11", "3.12"]
 
-@nox.session(venv_backend="conda", python=["3.10", "3.11", "3.12"], reuse_venv=False)
-def test_install(session):
+
+@nox.session(venv_backend="conda", python=SUPPORTED_PYTHON_VERSIONS, reuse_venv=False)
+def compatibility(session):
     session.install("-e", ".", silent=True)
     pip_show_output = session.run("pip", "show", "dojo", silent=True)
     if "not found" in pip_show_output:
         raise Exception(
             "Missing dojo package, this means installation probably failed."
         )
+
+
+@nox.session(venv_backend="conda", python=SUPPORTED_PYTHON_VERSIONS, reuse_venv=False)
+def unit_tests(session):
+    session.install("-e", ".", silent=True)
+    session.install("pytest", silent=True)
+    session.run("pytest")
