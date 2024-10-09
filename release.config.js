d85a067c34941e9acec161ecc6aa672a3489f38c
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-10-09 23:49:29 +0800

    feat!: semantic release

--- release.config.js
+++ release.config.js
@@ -1,8 +1,38 @@
 module.exports = {
   branches: ["main"], // specifies which branches semantic-release should consider for making releases
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { breaking: true, release: "major" },
+          { type: "feat", release: "minor" },
+          { type: "feature", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "build", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "revert", release: "patch" },
+        ],
+        parserOpts: {
+          noteKeywords: [
+            "BREAKING CHANGE",
+            "BREAKING CHANGES",
+            "BREAKING",
+            "MAJOR",
+          ],
+        },
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
     "@semantic-release/changelog",
     [
       "@semantic-release/git",
